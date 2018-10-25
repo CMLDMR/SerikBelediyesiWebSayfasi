4a3de66a9b2c7c708607c3484d09d3afcada4559
Cemal Demir <CMLDMR@users.noreply.github.com>
2018-10-25 17:47:58 +0300

    AnonsWidget Eklendi.

---
+++ SerikBelediyesiWebSayfasi/src/anonswidget.cpp
@@ -0,0 +1,6 @@
+#include "anonswidget.h"
+
+AnonsWidget::AnonsWidget()
+{
+
+}
---
+++ SerikBelediyesiWebSayfasi/src/anonswidget.h
@@ -0,0 +1,12 @@
+#ifndef ANONSWIDGET_H
+#define ANONSWIDGET_H
+
+#include <QtCore/qglobal.h>
+
+class AnonsWidget : public ContainerWidget
+{
+public:
+    AnonsWidget();
+};
+
+#endif // ANONSWIDGET_H
