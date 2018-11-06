b53f60fe418e52c09c64cf12bdfc0cbea01875b0
Cemal Demir <CMLDMR@users.noreply.github.com>
2018-11-06 15:57:56 +0300

    DataBaseWidget Created.

---
+++ SerikBelediyesiWebSayfasi/BaseClass/databasewidget.cpp
@@ -0,0 +1,6 @@
+#include "databasewidget.h"
+
+DataBaseWidget::DataBaseWidget()
+{
+
+}
---
+++ SerikBelediyesiWebSayfasi/BaseClass/databasewidget.h
@@ -0,0 +1,12 @@
+#ifndef DATABASEWIDGET_H
+#define DATABASEWIDGET_H
+
+#include <QtCore/qglobal.h>
+
+class DataBaseWidget : public WContainerWidget
+{
+public:
+    DataBaseWidget();
+};
+
+#endif // DATABASEWIDGET_H
