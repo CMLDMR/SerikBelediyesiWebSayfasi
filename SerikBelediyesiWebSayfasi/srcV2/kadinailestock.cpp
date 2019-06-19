e23f6f1b135d5f732fb054c606b65e0fd0431b67
Cemal DEMİR <cemaldemir07@gmail.com>
2019-06-19 09:28:56 +0300

    Kadın ve Aile Hizmetleri Müdürlüğü Stock Sınıflarını Eklendi.

---
+++ SerikBelediyesiWebSayfasi/srcV2/kadinailestock.cpp
@@ -0,0 +1,6 @@
+#include "kadinailestock.h"
+
+KadinAileStock::KadinAileStock()
+{
+
+}
---
+++ SerikBelediyesiWebSayfasi/srcV2/kadinailestock.h
@@ -0,0 +1,11 @@
+#ifndef KADINAILESTOCK_H
+#define KADINAILESTOCK_H
+
+
+class KadinAileStock : public ContainerWidget
+{
+public:
+    KadinAileStock();
+};
+
+#endif // KADINAILESTOCK_H
