4c0820c3045594c808924a6006abde12485fd113
Cemal DEMİR <cemaldemir07@gmail.com>
2019-06-28 11:06:43 +0300

    Giriş Çıkış Widget Dosyası Ayrıldı.

---
+++ SerikBelediyesiWebSayfasi/srcV2/girisCikisWidget/giriscikiswidget.cpp
@@ -0,0 +1,6 @@
+#include "giriscikiswidget.h"
+
+GirisCikisWidget::GirisCikisWidget()
+{
+
+}
---
+++ SerikBelediyesiWebSayfasi/srcV2/girisCikisWidget/giriscikiswidget.h
@@ -0,0 +1,11 @@
+#ifndef GIRISCIKISWIDGET_H
+#define GIRISCIKISWIDGET_H
+
+
+class GirisCikisWidget
+{
+public:
+    GirisCikisWidget();
+};
+
+#endif // GIRISCIKISWIDGET_H
