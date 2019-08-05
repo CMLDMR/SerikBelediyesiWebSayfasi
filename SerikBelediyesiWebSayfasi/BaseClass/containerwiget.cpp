92a7a69574c5e0ef7a3cc97fe09a4a6ddc41c63d
Cemal DEMİR <cemaldemir07@gmail.com>
2019-08-05 13:16:54 +0300

    Modified ContainerWidget.

--- SerikBelediyesiWebSayfasi/BaseClass/containerwiget.cpp
+++ SerikBelediyesiWebSayfasi/BaseClass/containerwiget.cpp
@@ -4,3 +4,40 @@ ContainerWiget::ContainerWiget()
 {
 
 }
+
+void ContainerWiget::setContainerStyle(ContainerWiget::ContainerStyleType type)
+{
+    switch (type) {
+    case ROW:
+        this->addStyleClass(Bootstrap::Grid::row);
+        break;
+
+    case CONTAINERFLUID:
+        this->addStyleClass(Bootstrap::Grid::container_fluid);
+        break;
+    default:
+        break;
+    }
+}
+
+void ContainerWiget::showMessage(std::string title, std::string msg, std::string btnText)
+{
+    auto messageBox = this->addChild(
+                Wt::cpp14::make_unique<Wt::WMessageBox>
+                (title,
+                 msg,
+                 Wt::Icon::Information, StandardButton::Ok));
+    if( btnText != "Tamam" )
+    {
+        auto btn = messageBox->button(StandardButton::Ok);
+        btn->setText(btnText);
+    }
+
+
+        messageBox->buttonClicked().connect([=] {
+            this->removeChild(messageBox);
+        });
+
+        messageBox->webWidget()->setZIndex(1000);
+        messageBox->show();
+}
--- SerikBelediyesiWebSayfasi/BaseClass/containerwiget.h
+++ SerikBelediyesiWebSayfasi/BaseClass/containerwiget.h
@@ -1,11 +1,34 @@
 #ifndef CONTAINERWIGET_H
 #define CONTAINERWIGET_H
 
+#include "SerikBelediyesiWebSayfasi/BaseClass/wtheaders.h"
+
+
 
 class ContainerWiget : public WContainerWidget
 {
+
+
+
+
 public:
     ContainerWiget();
+
+
+
+    enum ContainerStyleType
+    {
+        ROW = 0,
+        CONTAINERFLUID
+    };
+
+    void setContainerStyle(ContainerStyleType type);
+
+
+    void showMessage( std::string title , std::string msg , std::string btnText = "Tamam");
+
+
+
 };
 
 #endif // CONTAINERWIGET_H
