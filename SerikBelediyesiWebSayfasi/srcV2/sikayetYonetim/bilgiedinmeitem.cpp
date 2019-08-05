039ae71b48df692c64056f142339d19542811fa0
Cemal DEMİR <cemaldemir07@gmail.com>
2019-08-05 14:23:08 +0300

    BilgiEdinmeItem Class added.

---
+++ SerikBelediyesiWebSayfasi/srcV2/sikayetYonetim/bilgiedinmeitem.cpp
@@ -0,0 +1,6 @@
+#include "bilgiedinmeitem.h"
+
+BilgiEdinmeItem::BilgiEdinmeItem()
+{
+
+}
---
+++ SerikBelediyesiWebSayfasi/srcV2/sikayetYonetim/bilgiedinmeitem.h
@@ -0,0 +1,11 @@
+#ifndef BILGIEDINMEITEM_H
+#define BILGIEDINMEITEM_H
+
+
+class BilgiEdinmeItem : public DBClass
+{
+public:
+    BilgiEdinmeItem();
+};
+
+#endif // BILGIEDINMEITEM_H
