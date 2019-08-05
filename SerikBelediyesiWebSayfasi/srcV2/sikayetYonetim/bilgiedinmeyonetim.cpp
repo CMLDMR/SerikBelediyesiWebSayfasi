012a22d70cfbb581edbe730961b4a1e59832234a
Cemal DEMİR <cemaldemir07@gmail.com>
2019-08-05 15:41:55 +0300

    Implemented Constructor.

--- SerikBelediyesiWebSayfasi/srcV2/sikayetYonetim/bilgiedinmeyonetim.cpp
+++ SerikBelediyesiWebSayfasi/srcV2/sikayetYonetim/bilgiedinmeyonetim.cpp
@@ -6,4 +6,57 @@ BilgiEdinmeYonetim::BilgiEdinmeYonetim(mongocxx::database *_db, const bsoncxx::d
     :DBClass (_db),UserClass (_userValue)
 {
     setContainerStyle(ContainerStyleType::ROW);
+
+    auto list = BilgiEdinmeItem::GetList(this->db());
+
+    for( auto item : list )
+    {
+        if( item )
+        {
+            auto container = addWidget(cpp14::make_unique<WContainerWidget>());
+            container->addStyleClass(Bootstrap::Grid::col_full_12);
+            container->addWidget(cpp14::make_unique<BilgiEdinmeWidget>(item.get()));
+        }
+    }
+
+}
+
+BilgiEdinmeWidget::BilgiEdinmeWidget( const BilgiEdinmeItem *_item)
+    :DBClass (_item->db()),item(_item)
+{
+    setWidth(WLength("100%"));
+    setContainerStyle(ContainerStyleType::ROW);
+    addStyleClass(Bootstrap::ImageShape::img_thumbnail);
+    decorationStyle().setCursor(Cursor::PointingHand);
+    setMargin(10,Side::Top);
+
+    {
+        auto container = addWidget(cpp14::make_unique<WContainerWidget>());
+        container->addStyleClass(Bootstrap::Grid::Large::col_lg_4);
+        container->addWidget(cpp14::make_unique<WText>(this->item->Konu()));
+    }
+
+    {
+        auto container = addWidget(cpp14::make_unique<WContainerWidget>());
+        container->addStyleClass(Bootstrap::Grid::Large::col_lg_4);
+        container->addWidget(cpp14::make_unique<WText>(this->item->Tarih()));
+    }
+
+    {
+        auto container = addWidget(cpp14::make_unique<ContainerWiget>());
+        container->addStyleClass(Bootstrap::Grid::Large::col_lg_4);
+        if( this->item->cevaplandi() )
+        {
+            container->addWidget(cpp14::make_unique<WText>("Cevaplandı"));
+            setAttributeValue(Style::style,Style::background::color::rgb(this->getRandom(125,150),
+                                                                                    this->getRandom(225,250),
+                                                                                    this->getRandom(125,150)));
+        }else{
+            container->addWidget(cpp14::make_unique<WText>("Cevaplanmadı"));
+            setAttributeValue(Style::style,Style::background::color::rgb(this->getRandom(225,250),
+                                                                                    this->getRandom(125,150),
+                                                                                    this->getRandom(125,150)));
+        }
+    }
+
 }
--- SerikBelediyesiWebSayfasi/srcV2/sikayetYonetim/bilgiedinmeyonetim.h
+++ SerikBelediyesiWebSayfasi/srcV2/sikayetYonetim/bilgiedinmeyonetim.h
@@ -4,11 +4,27 @@
 #include "SerikBelediyesiWebSayfasi/BaseClass/containerwiget.h"
 #include "SerikBelediyesiWebSayfasi/BaseClass/dbclass.h"
 #include "SerikBelediyesiWebSayfasi/BaseClass/wtheaders.h"
+#include "bilgiedinmeitem.h"
+
+
 
 class BilgiEdinmeYonetim : public ContainerWiget , public DBClass , public UserClass
 {
 public:
     BilgiEdinmeYonetim(mongocxx::database* _db , const bsoncxx::document::value &_userValue );
 };
+
+
+
+class BilgiEdinmeWidget : public ContainerWiget , public DBClass
+{
+public:
+    explicit BilgiEdinmeWidget(const BilgiEdinmeItem *_item);
+
+private:
+
+    const BilgiEdinmeItem *item;
+};
+
 
 #endif // BILGIEDINMEYONETIM_H
