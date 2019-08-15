4f88b82e11d359ee95dae6123a524e42840ecc2f
Cemal DEMİR <cemaldemir07@gmail.com>
2019-08-15 17:25:07 +0300

    Kısmi Olarak Tamamlandı.

--- SerikBelediyesiWebSayfasi/srcV2/sikayetYonetim/sikayetlistitemwidget.cpp
+++ SerikBelediyesiWebSayfasi/srcV2/sikayetYonetim/sikayetlistitemwidget.cpp
@@ -1,6 +1,28 @@
 #include "sikayetlistitemwidget.h"
 
-SikayetListItemWidget::SikayetListItemWidget()
-{
 
+
+
+SikayetListItemWidget::SikayetListItemWidget(bsoncxx::oid _oid, const std::string durum, const std::string tarih, const std::string mahalle, const std::string birim, const std::string kategori, const std::string adsoyad)
+{
+    mOid = _oid;
+    this->setContainerStyle(ContainerStyleType::ROW);
+    this->setWidth(WLength("100%"));
+    this->addStyleClass(Bootstrap::ImageShape::img_thumbnail);
+    this->setAttributeValue(Style::style,Style::background::color::rgba(this->getRandom(175,225),this->getRandom(175,225),this->getRandom(175,225)));
+
+    {
+        auto container = addWidget(cpp14::make_unique<WContainerWidget>());
+        container->addStyleClass(Bootstrap::Grid::Large::col_lg_2+
+                                 Bootstrap::Grid::Medium::col_md_2+
+                                 Bootstrap::Grid::Small::col_sm_4+
+                                 Bootstrap::Grid::ExtraSmall::col_xs_6);
+        container->addStyleClass(Bootstrap::ImageShape::img_thumbnail);
+        container->addWidget(cpp14::make_unique<WText>(durum));
+    }
+}
+
+Signal<bsoncxx::oid> &SikayetListItemWidget::ClickItem()
+{
+    return _ClickItem;
 }
--- SerikBelediyesiWebSayfasi/srcV2/sikayetYonetim/sikayetlistitemwidget.h
+++ SerikBelediyesiWebSayfasi/srcV2/sikayetYonetim/sikayetlistitemwidget.h
@@ -1,11 +1,33 @@
 #ifndef SIKAYETLISTITEMWIDGET_H
 #define SIKAYETLISTITEMWIDGET_H
 
+#include "SerikBelediyesiWebSayfasi/BaseClass/containerwiget.h"
 
 class SikayetListItemWidget : public ContainerWiget
 {
 public:
-    SikayetListItemWidget();
+    SikayetListItemWidget(bsoncxx::oid _oid ,
+                          const std::string durum,
+                          const std::string tarih,
+                          const std::string mahalle,
+                          const std::string birim,
+                          const std::string kategori,
+                          const std::string adsoyad);
+
+
+    Signal<bsoncxx::oid> &ClickItem();
+
+private:
+
+    Signal<bsoncxx::oid> _ClickItem;
+
+    bsoncxx::oid mOid;
+    std::string mDurum;
+    std::string mTarih;
+    std::string mMahalle;
+    std::string mBirim;
+    std::string mKategori;
+    std::string mAdSoyad;
 };
 
 #endif // SIKAYETLISTITEMWIDGET_H
