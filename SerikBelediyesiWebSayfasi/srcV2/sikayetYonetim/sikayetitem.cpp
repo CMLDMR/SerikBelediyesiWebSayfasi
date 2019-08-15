8b8c3ca1ecf3549e5019f5650b28fbcd9ed7ab87
Cemal DEMİR <cemaldemir07@gmail.com>
2019-08-15 16:42:04 +0300

    Kısmi Olarak Tamamlandı.

--- SerikBelediyesiWebSayfasi/srcV2/sikayetYonetim/sikayetitem.cpp
+++ SerikBelediyesiWebSayfasi/srcV2/sikayetYonetim/sikayetitem.cpp
@@ -1,6 +1,31 @@
 #include "sikayetitem.h"
 
-SikayetItem::SikayetItem()
+
+
+boost::optional<Sikayet::SikayetItem> Sikayet::SikayetItem::Create_Sikayet(mongocxx::database *_db)
+{
+    SikayetItem item(_db);
+    if( item.isValid() )
+    {
+        return std::move(item);
+    }else{
+        return boost::none;
+    }
+}
+
+QVector<Sikayet::SikayetItem *> Sikayet::SikayetItem::GetList(mongocxx::database *_db,
+                                                              bsoncxx::builder::basic::document filter,
+                                                              mongocxx::options::find findOptions)
+{
+    QVector<Sikayet::SikayetItem*> list;
+
+
+
+    return list;
+}
+
+Sikayet::SikayetItem::SikayetItem(mongocxx::database *_db)
+    :ItemBase (_db,KEY::collection)
 {
 
 }
--- SerikBelediyesiWebSayfasi/srcV2/sikayetYonetim/sikayetitem.h
+++ SerikBelediyesiWebSayfasi/srcV2/sikayetYonetim/sikayetitem.h
@@ -1,11 +1,48 @@
 #ifndef SIKAYETITEM_H
 #define SIKAYETITEM_H
 
+#include "SerikBelediyesiWebSayfasi/BaseClass/itembase.h"
+#include <QVector>
 
+
+namespace Sikayet {
+
+
+namespace KEY {
+static const std::string collection{"Sikayet"};
+
+static const std::string julianDay{"julianDay"};
+static const std::string konu{"Konu"};
+static const std::string mahalle{"Mahalle"};
+static const std::string birim{"Birim"};
+static const std::string durum{"Durum"};
+static const std::string talepSahibi{"Şikayet/Talep Sahibi"};
+static const std::string adSoyad{"adSoyad"};
+static const std::string saat{"Saat"};
+static const std::string tarih{"Tarih"};
+static const std::string tamAdress{"Tam Adres"};
+static const std::string cagriMerkeziPersonel{"CagriMerkeziPersoneli"};
+static const std::string kategori{"Kategori"};
+static const std::string asama{"ASAMA"};
+static const std::string gorevli{"Görevli Personeller"};
+
+}
 class SikayetItem : public ItemBase
 {
 public:
-    SikayetItem();
+    boost::optional<SikayetItem> Create_Sikayet(mongocxx::database* _db);
+
+    static QVector<SikayetItem*> GetList(mongocxx::database* _db,
+                                  bsoncxx::builder::basic::document filter = document{} ,
+                                  mongocxx::options::find findOptions = mongocxx::options::find{} );
+
+
+private:
+    SikayetItem(mongocxx::database* _db );
 };
+
+}
+
+
 
 #endif // SIKAYETITEM_H
