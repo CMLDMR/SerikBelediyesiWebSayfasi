f3f3fc68913484028bb1d99533424f0482190a35
Cemal DEMİR <cemaldemir07@gmail.com>
2019-08-15 12:40:16 +0300

    ItemBase Sınıfı Tamamlandı.

--- SerikBelediyesiWebSayfasi/BaseClass/itembase.cpp
+++ SerikBelediyesiWebSayfasi/BaseClass/itembase.cpp
@@ -1,6 +1,131 @@
 #include "itembase.h"
 
-ItemBase::ItemBase()
+ItemBase::ItemBase(mongocxx::database *_db, const std::string &collection)
+    : DBClass (_db),
+      collectionName(collection),
+      mIsValid(true),
+    doc(document{})
 {
+    try {
+        auto ins = this->db()->collection(collectionName).insert_one(doc.view());
+        if( ins )
+        {
+            this->mOid = ins.value().inserted_id().get_oid().value;
+        }else{
+            mIsValid = false;
+        }
+    } catch (mongocxx::exception &e) {
+        std::cout << "ERROR: " << __LINE__ << " " << __FUNCTION__ << " " << e.what() << std::endl;
+        mIsValid = false;
+    }
+}
+
+ItemBase::ItemBase(mongocxx::database *_db, const std::string &collection, bsoncxx::document::view &_view)
+    : DBClass (_db),
+      collectionName(collection),
+      mIsValid(true)
+{
+    this->doc = document{};
+    for( auto it = _view.cbegin() ; it != _view.cend() ; it++ )
+    {
+
+        try {
+            this->doc.append(kvp(it->key(),it->get_value()));
+        } catch (bsoncxx::exception &e) {
+            std::cout << "ERROR: " << __LINE__ << " " << __FUNCTION__ << " " << e.what() << std::endl;
+            mIsValid = false;
+        }
+
+    }
+
+    if( mIsValid )
+    {
+        try {
+            mOid = this->doc.view()["_id"].get_oid().value;
+        } catch (bsoncxx::exception &e) {
+            std::cout << "ERROR: " << __LINE__ << " " << __FUNCTION__ << " " << e.what() << std::endl;
+            mIsValid = false;
+        }
+    }
+
+
+}
+
+//ItemBase::~ItemBase()
+//{
+
+//    std::cout << "Destructor ItemBase" << std::endl;
+//}
+
+boost::optional<bsoncxx::builder::basic::document> ItemBase::filter()
+{
+    auto filter_ = document{};
+    try {
+        filter_.append(kvp("_id",mOid));
+    } catch (bsoncxx::exception &e) {
+        std::cout << "ERROR: " << __LINE__ << " " << __FUNCTION__ << " " << e.what() << std::endl;
+        return boost::none;
+    }
+    return std::move(filter_);
+}
+
+QStringList ItemBase::keyList()
+{
+    QStringList list;
+    for (auto it = this->doc.view().cbegin() ; it != this->doc.view().cend() ; it++ ) {
+        list.push_back(it->key().to_string().c_str());
+    }
+    return list;
+}
+
+bsoncxx::document::view ItemBase::view()
+{
+    return this->doc.view();
+}
+
+bool ItemBase::deleteItem()
+{
+    auto _filter = this->filter();
+
+    if( _filter )
+    {
+        try {
+            auto del = this->db()->collection(collectionName).delete_one(_filter.value().view());
+            if( del )
+            {
+                if( del.value().deleted_count() )
+                {
+                    return true;
+                }
+            }
+        } catch (mongocxx::exception &e) {
+            std::cout << "ERROR: " << __LINE__ << " " << __FUNCTION__ << " " << e.what() << std::endl;
+        }
+    }
+
+    return false;
+
+}
+
+bsoncxx::oid ItemBase::oid() const
+{
+    return mOid;
+}
+
+bool ItemBase::isValid() const
+{
+    return mIsValid;
+}
+
+
+boost::optional<bsoncxx::types::value> ItemBase::Element(const std::string &key)
+{
+
+    try {
+        return this->view()[key].get_value();
+    } catch (bsoncxx::exception &e) {
+        std::cout << "ERROR: " << __LINE__ << " " << __FUNCTION__ << " "<<key << ": " << e.what() << std::endl;
+        return boost::none;
+    }
 
 }
--- SerikBelediyesiWebSayfasi/BaseClass/itembase.h
+++ SerikBelediyesiWebSayfasi/BaseClass/itembase.h
@@ -2,10 +2,158 @@
 #define ITEMBASE_H
 
 
+#include "dbclass.h"
+#include <QStringList>
+
+
 class ItemBase : public DBClass
 {
 public:
-    ItemBase();
+    explicit ItemBase(mongocxx::database* _db, const std::string &collection);
+    ItemBase(mongocxx::database* _db , const std::string &collection , bsoncxx::document::view &_view);
+
+//    virtual ~ItemBase();
+
+    template<typename T>
+    bool addElement( const std::string &key , const T &value ){
+        return this->setElement(key,value);
+    }
+
+    bool addElement( const std::string &key , const QString &value ){
+        return this->setElement(key,value.toStdString());
+    }
+
+    boost::optional<bsoncxx::types::value> Element( const std::string &key);
+
+    bool isValid() const;
+
+    bsoncxx::oid oid() const;
+
+    boost::optional<document> filter();
+
+
+    QStringList keyList();
+
+    bsoncxx::document::view view();
+
+
+    bool deleteItem();
+
+
+private:
+
+    document doc;
+
+    std::string collectionName;
+
+    bool mIsValid;
+
+    bsoncxx::oid mOid;
+
+    template<typename T>
+    bool setElement( const std::string &key , const T& value )
+    {
+        bool succed = true;
+
+        auto f = this->doc.view().find(key);
+        if( *f ){
+
+            auto doc = document{};
+
+
+
+            for( auto _f : this->doc.view() )
+            {
+                if( key != _f.key() )
+                {
+                    try {
+                        doc.append(kvp(_f.key(),_f.get_value()));
+                    } catch (bsoncxx::exception &e) {
+                        std::cout << "ERROR: " << __LINE__ << " " << __FUNCTION__ << " " << e.what() << std::endl;
+                        succed = false;
+                    }
+                }
+            }
+
+            if( succed ){
+
+                this->doc.clear();
+
+                for( auto _f : doc.view() )
+                {
+                    try {
+                        doc.append(kvp(_f.key(),_f.get_value()));
+                    } catch (bsoncxx::exception &e) {
+                        std::cout << "ERROR: " << __LINE__ << " " << __FUNCTION__ << " " << e.what() << std::endl;
+                        succed = false;
+                    }
+                }
+
+                try {
+                    doc.append(kvp(key,value));
+                } catch (bsoncxx::exception &e) {
+                    std::cout << "ERROR: " << __LINE__ << " " << __FUNCTION__ << " " << e.what() << std::endl;
+                    succed = false;
+                }
+            }
+
+
+        }else{
+            try {
+                doc.append(kvp(key,value));
+            } catch (bsoncxx::exception &e) {
+                std::cout << "ERROR: " << __LINE__ << " " << __FUNCTION__ << " " << e.what() << std::endl;
+                succed = false;
+            }
+        }
+
+
+        if( succed )
+        {
+            auto filter = document{};
+
+            try {
+                filter.append(kvp("_id",mOid));
+            } catch (bsoncxx::exception &e) {
+                std::cout << "ERROR: " << __LINE__ << " " << __FUNCTION__ << " " << e.what() << std::endl;
+                succed = false;
+            }
+
+            auto setDoc = document{};
+
+            try {
+                setDoc.append(kvp("$set",this->doc.view()));
+            } catch (bsoncxx::exception &e) {
+                std::cout << "ERROR: " << __LINE__ << " " << __FUNCTION__ << " " << e.what() << std::endl;
+                succed = false;
+            }
+
+
+            if( succed ){
+                try {
+                    auto upt = this->db()->collection(collectionName).update_one(filter.view(),setDoc.view());
+                    if( upt )
+                    {
+                        if( upt.value().modified_count() )
+                        {
+
+                        }else{
+                            succed = false;
+                        }
+                    }else{
+                        succed = false;
+                    }
+                } catch (mongocxx::exception &e) {
+                    std::cout << "ERROR: " << __LINE__ << " " << __FUNCTION__ << " " << e.what() << std::endl;
+                    succed = false;
+                }
+            }
+
+        }
+        return succed;
+    }
+
 };
+
 
 #endif // ITEMBASE_H
