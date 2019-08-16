f7c56f6b3f8edec577e5ae443e4c181f279c04cc
Cemal DEMİR <cemaldemir07@gmail.com>
2019-08-16 09:44:19 +0300

    Constructor implement edildi.

--- SerikBelediyesiWebSayfasi/srcV2/sikayetYonetim/sikayetlistitemwidget.cpp
+++ SerikBelediyesiWebSayfasi/srcV2/sikayetYonetim/sikayetlistitemwidget.cpp
@@ -6,7 +6,7 @@
 SikayetListItemWidget::SikayetListItemWidget(bsoncxx::oid _oid, const std::string durum, const std::string tarih, const std::string mahalle, const std::string birim, const std::string kategori, const std::string adsoyad)
 {
     mOid = _oid;
-    this->setContainerStyle(ContainerStyleType::ROW);
+    this->addStyleClass(Bootstrap::Grid::col_full_12);
     this->setWidth(WLength("100%"));
     this->addStyleClass(Bootstrap::ImageShape::img_thumbnail);
     this->setAttributeValue(Style::style,Style::background::color::rgba(this->getRandom(175,225),this->getRandom(175,225),this->getRandom(175,225)));
@@ -17,8 +17,56 @@ SikayetListItemWidget::SikayetListItemWidget(bsoncxx::oid _oid, const std::strin
                                  Bootstrap::Grid::Medium::col_md_2+
                                  Bootstrap::Grid::Small::col_sm_4+
                                  Bootstrap::Grid::ExtraSmall::col_xs_6);
-        container->addStyleClass(Bootstrap::ImageShape::img_thumbnail);
         container->addWidget(cpp14::make_unique<WText>(durum));
+    }
+    {
+        auto container = addWidget(cpp14::make_unique<WContainerWidget>());
+        container->addStyleClass(Bootstrap::Grid::Large::col_lg_2+
+                                 Bootstrap::Grid::Medium::col_md_2+
+                                 Bootstrap::Grid::Small::col_sm_4+
+                                 Bootstrap::Grid::ExtraSmall::col_xs_6);
+        container->setAttributeValue(Style::style,Style::Border::left::border("2px solid white"));
+        container->addWidget(cpp14::make_unique<WText>(tarih));
+    }
+    {
+        auto container = addWidget(cpp14::make_unique<WContainerWidget>());
+        container->addStyleClass(Bootstrap::Grid::Large::col_lg_2+
+                                 Bootstrap::Grid::Medium::col_md_2+
+                                 Bootstrap::Grid::Small::col_sm_4+
+                                 Bootstrap::Grid::ExtraSmall::col_xs_6);
+        container->setAttributeValue(Style::style,Style::Border::left::border("2px solid white"));
+
+        container->addWidget(cpp14::make_unique<WText>(mahalle));
+    }
+    {
+        auto container = addWidget(cpp14::make_unique<WContainerWidget>());
+        container->addStyleClass(Bootstrap::Grid::Large::col_lg_2+
+                                 Bootstrap::Grid::Medium::col_md_2+
+                                 Bootstrap::Grid::Small::col_sm_4+
+                                 Bootstrap::Grid::ExtraSmall::col_xs_6);
+        container->setAttributeValue(Style::style,Style::Border::left::border("2px solid white"));
+
+        container->addWidget(cpp14::make_unique<WText>(birim));
+    }
+    {
+        auto container = addWidget(cpp14::make_unique<WContainerWidget>());
+        container->addStyleClass(Bootstrap::Grid::Large::col_lg_2+
+                                 Bootstrap::Grid::Medium::col_md_2+
+                                 Bootstrap::Grid::Small::col_sm_4+
+                                 Bootstrap::Grid::ExtraSmall::col_xs_6);
+        container->setAttributeValue(Style::style,Style::Border::left::border("2px solid white"));
+
+        container->addWidget(cpp14::make_unique<WText>(kategori));
+    }
+    {
+        auto container = addWidget(cpp14::make_unique<WContainerWidget>());
+        container->addStyleClass(Bootstrap::Grid::Large::col_lg_2+
+                                 Bootstrap::Grid::Medium::col_md_2+
+                                 Bootstrap::Grid::Small::col_sm_4+
+                                 Bootstrap::Grid::ExtraSmall::col_xs_6);
+        container->setAttributeValue(Style::style,Style::Border::left::border("2px solid white"));
+
+        container->addWidget(cpp14::make_unique<WText>(adsoyad));
     }
 }
 
