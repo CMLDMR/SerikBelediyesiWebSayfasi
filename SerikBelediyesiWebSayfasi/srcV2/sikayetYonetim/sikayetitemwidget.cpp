cdb366d561772ee6e934c76d60b1b37e56b6ba84
Cemal DEMİR <cemaldemir07@gmail.com>
2019-08-19 15:17:49 +0300

    Controller Widget Tamamlandı.

--- SerikBelediyesiWebSayfasi/srcV2/sikayetYonetim/sikayetitemwidget.cpp
+++ SerikBelediyesiWebSayfasi/srcV2/sikayetYonetim/sikayetitemwidget.cpp
@@ -5,10 +5,13 @@ SikayetItemWidget::SikayetItemWidget(mongocxx::database *_db, UserClass &userVal
 {
     this->initHeader();
     this->initContent();
+    this->initController();
 }
 
 void SikayetItemWidget::initHeader()
 {
+//    Header()->addStyleClass(Bootstrap::ImageShape::img_thumbnail);
+
     {
         auto container = this->Header()->addWidget(cpp14::make_unique<WContainerWidget>());
         container->addStyleClass(Bootstrap::Grid::Large::col_lg_6+
@@ -141,6 +144,220 @@ void SikayetItemWidget::initContent()
 void SikayetItemWidget::initController()
 {
     this->Footer()->clear();
+    this->Footer()->setMargin(25,Side::Top);
+
+    this->Footer()->setPadding(10,AllSides);
+
+    Footer()->addStyleClass(Bootstrap::ImageShape::img_thumbnail);
+    Footer()->setWidth(WLength("100%"));
+    Footer()->setAttributeValue(Style::style,Style::background::color::rgba(this->getRandom(150,170),
+                                                                            this->getRandom(170,190),
+                                                                            this->getRandom(190,210)));
+
+
+
+    {
+        auto container = Footer()->addWidget(cpp14::make_unique<WContainerWidget>());
+        container->addStyleClass(Bootstrap::Grid::col_full_12+
+                                 Bootstrap::ImageShape::img_thumbnail+
+                                 Bootstrap::ContextualBackGround::bg_info);
+        container->setMargin(15,Side::Top|Side::Bottom);
+
+        auto rContainer = container->addWidget(cpp14::make_unique<WContainerWidget>());
+        rContainer->addStyleClass(Bootstrap::Grid::row);
+
+        {
+            auto text = rContainer->addWidget(cpp14::make_unique<WText>("Birim Değiştir"));
+            text->addStyleClass(Bootstrap::Grid::col_full_12);
+            text->setAttributeValue(Style::style,Style::font::size::s12px);
+        }
+
+        {
+            auto _container = rContainer->addWidget(cpp14::make_unique<WContainerWidget>());
+            _container->addStyleClass(Bootstrap::Grid::Large::col_lg_10+
+                                      Bootstrap::Grid::Medium::col_md_10+
+                                      Bootstrap::Grid::Small::col_sm_10+
+                                      Bootstrap::Grid::ExtraSmall::col_xs_9);
+            auto comboBox = _container->addWidget(cpp14::make_unique<WComboBox>());
+
+            auto filter = document{};
+
+            try {
+                filter.append(kvp("Haberleşme Kodu",make_document(kvp("$ne","0"))));
+            } catch (bsoncxx::exception &e) {
+                std::string str = "ERROR: " + std::to_string(__LINE__) + " " + __FUNCTION__ + " " + e.what();
+                std::cout << str << std::endl;
+            }
+
+            try {
+                auto cursor = this->db()->collection("Müdürlükler").find(filter.view());
+
+                for( auto doc : cursor )
+                {
+
+                    try {
+                        comboBox->addItem(doc["Birim"].get_utf8().value.to_string());
+                    } catch (bsoncxx::exception &e) {
+                        std::string str = "ERROR: " + std::to_string(__LINE__) + " " + __FUNCTION__ + " " + e.what();
+                        std::cout << str << std::endl;
+                    }
+
+                }
+
+            } catch (mongocxx::exception &e) {
+                std::string str = "ERROR: " + std::to_string(__LINE__) + " " + __FUNCTION__ + " " + e.what();
+                std::cout << str << std::endl;
+            }
+
+
+        }
+
+        {
+            auto _container = rContainer->addWidget(cpp14::make_unique<WContainerWidget>());
+            _container->addStyleClass(Bootstrap::Grid::Large::col_lg_2+
+                                      Bootstrap::Grid::Medium::col_md_2+
+                                      Bootstrap::Grid::Small::col_sm_2+
+                                      Bootstrap::Grid::ExtraSmall::col_xs_3);
+            auto setBirimBtn = _container->addWidget(cpp14::make_unique<WPushButton>("Değiştir"));
+            setBirimBtn->addStyleClass(Bootstrap::Button::Primary);
+        }
+
+
+
+
+
+        Footer()->addWidget(cpp14::make_unique<WBreak>());
+    }
+
+
+    {
+        auto container = Footer()->addWidget(cpp14::make_unique<WContainerWidget>());
+        container->addStyleClass(Bootstrap::Grid::col_full_12+
+                                 Bootstrap::ImageShape::img_thumbnail+
+                                 Bootstrap::ContextualBackGround::bg_info);
+        container->setMargin(15,Side::Top|Side::Bottom);
+
+        auto rContainer = container->addWidget(cpp14::make_unique<WContainerWidget>());
+        rContainer->addStyleClass(Bootstrap::Grid::row);
+
+        {
+            auto text = rContainer->addWidget(cpp14::make_unique<WText>("Kategori Değiştir"));
+            text->addStyleClass(Bootstrap::Grid::col_full_12);
+            text->setAttributeValue(Style::style,Style::font::size::s12px);
+        }
+
+        {
+            auto _container = rContainer->addWidget(cpp14::make_unique<WContainerWidget>());
+            _container->addStyleClass(Bootstrap::Grid::Large::col_lg_10+
+                                      Bootstrap::Grid::Medium::col_md_10+
+                                      Bootstrap::Grid::Small::col_sm_10+
+                                      Bootstrap::Grid::ExtraSmall::col_xs_9);
+            auto comboBox = _container->addWidget(cpp14::make_unique<WComboBox>());
+
+            auto filter = document{};
+
+            try {
+                auto cursor = this->db()->collection("SikayetKategorileri").find(filter.view());
+
+                for( auto doc : cursor )
+                {
+
+                    try {
+                        comboBox->addItem(doc["baslik"].get_utf8().value.to_string());
+                    } catch (bsoncxx::exception &e) {
+                        std::string str = "ERROR: " + std::to_string(__LINE__) + " " + __FUNCTION__ + " " + e.what();
+                        std::cout << str << std::endl;
+                    }
+
+                }
+
+            } catch (mongocxx::exception &e) {
+                std::string str = "ERROR: " + std::to_string(__LINE__) + " " + __FUNCTION__ + " " + e.what();
+                std::cout << str << std::endl;
+            }
+        }
+        {
+            auto _container = rContainer->addWidget(cpp14::make_unique<WContainerWidget>());
+            _container->addStyleClass(Bootstrap::Grid::Large::col_lg_2+
+                                      Bootstrap::Grid::Medium::col_md_2+
+                                      Bootstrap::Grid::Small::col_sm_2+
+                                      Bootstrap::Grid::ExtraSmall::col_xs_3);
+            auto setKategoriBtn = _container->addWidget(cpp14::make_unique<WPushButton>("Değiştir"));
+            setKategoriBtn->addStyleClass(Bootstrap::Button::Primary);
+        }
+
+
+        Footer()->addWidget(cpp14::make_unique<WBreak>());
+    }
+
+
+
+    {
+        auto container = Footer()->addWidget(cpp14::make_unique<WContainerWidget>());
+        container->addStyleClass(Bootstrap::Grid::col_full_12+
+                                 Bootstrap::ImageShape::img_thumbnail+
+                                 Bootstrap::ContextualBackGround::bg_info);
+        container->setMargin(15,Side::Top|Side::Bottom);
+
+        auto rContainer = container->addWidget(cpp14::make_unique<WContainerWidget>());
+        rContainer->addStyleClass(Bootstrap::Grid::row);
+
+        {
+            auto text = rContainer->addWidget(cpp14::make_unique<WText>("Durum Değiştir"));
+            text->addStyleClass(Bootstrap::Grid::col_full_12);
+            text->setAttributeValue(Style::style,Style::font::size::s12px);
+        }
+
+        {
+            auto _container = rContainer->addWidget(cpp14::make_unique<WContainerWidget>());
+            _container->addStyleClass(Bootstrap::Grid::Large::col_lg_10+
+                                      Bootstrap::Grid::Medium::col_md_10+
+                                      Bootstrap::Grid::Small::col_sm_10+
+                                      Bootstrap::Grid::ExtraSmall::col_xs_9);
+
+            auto comboBox = _container->addWidget(cpp14::make_unique<WComboBox>());
+            comboBox->addItem(Sikayet::KEY::DURUM::beklemede);
+            comboBox->addItem(Sikayet::KEY::DURUM::tekrarAcildi);
+            comboBox->addItem(Sikayet::KEY::DURUM::iptalEdildi);
+            comboBox->addItem(Sikayet::KEY::DURUM::teyitEdilmemis);
+            comboBox->addItem(Sikayet::KEY::DURUM::tamamlandi);
+            comboBox->addItem(Sikayet::KEY::DURUM::devamEdiyor);
+        }
+
+        {
+            auto _container = rContainer->addWidget(cpp14::make_unique<WContainerWidget>());
+            _container->addStyleClass(Bootstrap::Grid::Large::col_lg_2+
+                                      Bootstrap::Grid::Medium::col_md_2+
+                                      Bootstrap::Grid::Small::col_sm_2+
+                                      Bootstrap::Grid::ExtraSmall::col_xs_3);
+            auto setBirimBtn = _container->addWidget(cpp14::make_unique<WPushButton>("Değiştir"));
+            setBirimBtn->addStyleClass(Bootstrap::Button::Primary);
+        }
+
+
+        Footer()->addWidget(cpp14::make_unique<WBreak>());
+    }
+
+
+
+    {
+        auto text = Footer()->addWidget(cpp14::make_unique<WText>("Açıklama Ekle"));
+        text->addStyleClass(Bootstrap::Label::info);
+        text->setAttributeValue(Style::style,Style::font::size::s12px);
+    }
+
+
+
+
+    Footer()->addWidget(cpp14::make_unique<WBreak>());
+    auto text = this->Footer()->addWidget(cpp14::make_unique<WTextEdit>());
+    text->setWidth(WLength("100%"));
+    text->setHeight(250);
+
+
+    Footer()->addWidget(cpp14::make_unique<WBreak>());
+    auto PushBtn = Footer()->addWidget(cpp14::make_unique<WPushButton>("Ekle"));
+    PushBtn->addStyleClass(Bootstrap::Button::Primary);
 }
 
 AsamaItemWidget::AsamaItemWidget(bsoncxx::document::view &&view)
