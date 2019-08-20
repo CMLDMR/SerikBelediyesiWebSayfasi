13baa813f2d123c45d4e593a799e405f9b281ac1
Cemal DEMİR <cemaldemir07@gmail.com>
2019-08-20 15:53:50 +0300

    TC::TCItem Tamamlandı.

--- SerikBelediyesiWebSayfasi/baseItem/tcitem.cpp
+++ SerikBelediyesiWebSayfasi/baseItem/tcitem.cpp
@@ -1,6 +1,49 @@
 #include "tcitem.h"
 
-TCItem::TCItem()
+
+
+boost::optional<TC::TCItem> TC::TCItem::Create_TC(mongocxx::database *_db)
+{
+    TCItem item(_db);
+
+    if( item.isValid() )
+    {
+        return std::move(item);
+    }
+    return boost::none;
+}
+
+//QVector<TC::TCItem> TC::TCItem::GetList(mongocxx::database *_db,
+//                                        bsoncxx::builder::basic::document filter,
+//                                        mongocxx::options::find findOptions)
+//{
+//    QVector<TCItem> list;
+
+//    try {
+//        auto cursor = _db->collection(collection).find(filter.view(),findOptions);
+//    } catch (mongocxx::exception &e) {
+//        std::string str = "ERROR: " + std::to_string(__LINE__) + " " + __FUNCTION__ + " " + e.what();
+//        std::cout << str << std::endl;
+//    }
+
+
+//    return list;
+//}
+
+TC::TCItem::TCItem(mongocxx::database *_db)
+    :ItemBase (_db,collection)
+{
+
+}
+
+TC::TCItem::TCItem(mongocxx::database *_db, bsoncxx::document::view _view)
+    :ItemBase (_db,collection,_view)
+{
+
+}
+
+TC::TCItem::TCItem(mongocxx::database *_db, const std::string &_collection, bsoncxx::document::view _view)
+    :ItemBase (_db,_collection,_view)
 {
 
 }
--- SerikBelediyesiWebSayfasi/baseItem/tcitem.h
+++ SerikBelediyesiWebSayfasi/baseItem/tcitem.h
@@ -1,11 +1,33 @@
 #ifndef TCITEM_H
 #define TCITEM_H
 
+#include "SerikBelediyesiWebSayfasi/BaseClass/itembase.h"
+#include <QVector>
+
+namespace TC {
+static const std::string collection{"TC"};
+namespace KEY {
+
+}
+
 
 class TCItem : public ItemBase
 {
 public:
-    TCItem();
+    static boost::optional<TCItem> Create_TC(mongocxx::database* _db);
+
+
+
+
+//protected:
+    explicit TCItem(mongocxx::database* _db);
+    TCItem(mongocxx::database* _db , bsoncxx::document::view _view);
+    TCItem(mongocxx::database* _db , const std::string &_collection , bsoncxx::document::view _view);
 };
+
+
+}
+
+
 
 #endif // TCITEM_H
