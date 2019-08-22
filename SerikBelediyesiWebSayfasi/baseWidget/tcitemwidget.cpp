2e2796b8156a3d7ccb419a8fba19bf75e25b7ee0
Cemal DEMİR <cemaldemir07@gmail.com>
2019-08-22 10:16:03 +0300

    TCItemWidget Tanımlandı.

---
+++ SerikBelediyesiWebSayfasi/baseWidget/tcitemwidget.cpp
@@ -0,0 +1,6 @@
+#include "tcitemwidget.h"
+
+TCItemWidget::TCItemWidget()
+{
+
+}
---
+++ SerikBelediyesiWebSayfasi/baseWidget/tcitemwidget.h
@@ -0,0 +1,11 @@
+#ifndef TCITEMWIDGET_H
+#define TCITEMWIDGET_H
+
+
+class TCItemWidget : public TCItem
+{
+public:
+    TCItemWidget();
+};
+
+#endif // TCITEMWIDGET_H
