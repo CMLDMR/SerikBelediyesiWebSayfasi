2fae0dc2d43dcad879899275db9e0d22bf02483f
Cemal DEMİR <cemaldemir07@gmail.com>
2019-08-22 13:08:52 +0300

    Kapat Eklendi. Header ve Footer Background renklendirildi.

--- SerikBelediyesiWebSayfasi/BaseClass/dialog.h
+++ SerikBelediyesiWebSayfasi/BaseClass/dialog.h
@@ -1,11 +1,15 @@
 #ifndef DIALOG_H
 #define DIALOG_H
 
+#include "SerikBelediyesiWebSayfasi/BaseClass/containerwiget.h"
+#include "SerikBelediyesiWebSayfasi/BaseClass/dbclass.h"
 
-class Dialog : public WDialog
+class Dialog : public WDialog , public DBClass , public UserClass
 {
 public:
-    Dialog();
+    Dialog(mongocxx::database* _db , bsoncxx::document::value &userValue , const std::string &title = "");
+
+
 };
 
 #endif // DIALOG_H
