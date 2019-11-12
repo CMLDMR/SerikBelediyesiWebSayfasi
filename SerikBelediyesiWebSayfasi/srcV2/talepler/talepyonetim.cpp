0d4639b65e3bc8c371eb37893da0dba1a4b56dc0
Cemal DEMİR <cemaldemir07@gmail.com>
2019-11-12 13:43:23 +0300

    TalepYonetim Eklendi.

---
+++ SerikBelediyesiWebSayfasi/srcV2/talepler/talepyonetim.cpp
@@ -0,0 +1,6 @@
+#include "talepyonetim.h"
+
+TalepYonetim::TalepYonetim()
+{
+
+}
---
+++ SerikBelediyesiWebSayfasi/srcV2/talepler/talepyonetim.h
@@ -0,0 +1,11 @@
+#ifndef TALEPYONETIM_H
+#define TALEPYONETIM_H
+
+
+class TalepYonetim : public ContainerWidget
+{
+public:
+    TalepYonetim();
+};
+
+#endif // TALEPYONETIM_H
