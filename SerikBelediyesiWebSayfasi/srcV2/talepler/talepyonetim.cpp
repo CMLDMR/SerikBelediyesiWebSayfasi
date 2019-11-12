d2f63702be185f8cae166f3d4d2db8a4cfc08601
Cemal DEMİR <cemaldemir07@gmail.com>
2019-11-12 14:07:38 +0300

    Kontrol Menü Oluşturuldu.

--- SerikBelediyesiWebSayfasi/srcV2/talepler/talepyonetim.cpp
+++ SerikBelediyesiWebSayfasi/srcV2/talepler/talepyonetim.cpp
@@ -1,6 +1,60 @@
 #include "talepyonetim.h"
 
-TalepYonetim::TalepYonetim()
+TalepYonetim::TalepYonetim(mongocxx::database *_db, bsoncxx::document::value _user)
+    :ContainerWidget ("Talep/Şikayet Yönetimi"),
+      TalepManager (_db),
+      mUser(new User (_db,_user))
 {
 
+    this->initControlPanel ();
+
+}
+
+void TalepYonetim::initControlPanel()
+{
+    this->Header ()->clear ();
+
+    {
+
+        auto toolbarWidget = this->Header ()->addWidget (cpp14::make_unique<WToolBar>());
+
+        auto devamBtn = createButton("btn-primary", TalepKey::DurumKey::DevamEdiyor+" "+Bootstrap::Badges::badget(std::to_string(55)));
+        devamBtn->clicked().connect([=](){
+//            this->initTalepler(SBLDKeys::SikayetKey::durumType::devamediyor);
+        });
+        toolbarWidget->addButton(std::move(devamBtn));
+
+        auto tamamlandiBtn = createButton("btn-success", TalepKey::DurumKey::Tamamlandi+" "+Bootstrap::Badges::badget(std::to_string(44)));
+        tamamlandiBtn->clicked().connect([=](){
+//            this->initTalepler(SBLDKeys::SikayetKey::durumType::devamediyor);
+        });
+        toolbarWidget->addButton(std::move(tamamlandiBtn));
+
+        auto redEdildiBtn = createButton("btn-danger", TalepKey::DurumKey::RedEdildi+" "+Bootstrap::Badges::badget(std::to_string(22)));
+        redEdildiBtn->clicked().connect([=](){
+//            this->initTalepler(SBLDKeys::SikayetKey::durumType::devamediyor);
+        });
+        toolbarWidget->addButton(std::move(redEdildiBtn));
+
+        auto teyitEdilmemisBtn = createButton("btn-info", TalepKey::DurumKey::TeyitEdilmemis+" "+Bootstrap::Badges::badget(std::to_string(33)));
+        teyitEdilmemisBtn->clicked().connect([=](){
+//            this->initTalepler(SBLDKeys::SikayetKey::durumType::devamediyor);
+        });
+        toolbarWidget->addButton(std::move(teyitEdilmemisBtn));
+
+        auto beklemedeBtn = createButton("btn-warning", TalepKey::DurumKey::Beklemede+" "+Bootstrap::Badges::badget(std::to_string(11)));
+        beklemedeBtn->clicked().connect([=](){
+//            this->initTalepler(SBLDKeys::SikayetKey::durumType::devamediyor);
+        });
+        toolbarWidget->addButton(std::move(beklemedeBtn));
+    }
+}
+
+std::unique_ptr<WPushButton> TalepYonetim::createButton(const char *classname, const WString &name)
+{
+    auto button = Wt::cpp14::make_unique<Wt::WPushButton>();
+    button->setTextFormat(Wt::TextFormat::XHTML);
+    button->setText(name);
+    button->addStyleClass(classname);
+    return button;
 }
--- SerikBelediyesiWebSayfasi/srcV2/talepler/talepyonetim.h
+++ SerikBelediyesiWebSayfasi/srcV2/talepler/talepyonetim.h
@@ -1,11 +1,27 @@
 #ifndef TALEPYONETIM_H
 #define TALEPYONETIM_H
 
+#include "SerikBelediyesiWebSayfasi/BaseClass/containerwiget.h"
 
-class TalepYonetim : public ContainerWidget
+#include <vector>
+#include <memory>
+#include "db.h"
+#include "user.h"
+#include "Talep/talepmanager.h"
+
+class TalepYonetim : public ContainerWidget , public TalepManager
 {
 public:
-    TalepYonetim();
+    TalepYonetim(mongocxx::database* _db , bsoncxx::document::value _user);
+
+
+private:
+    User* mUser;
+
+    void initControlPanel();
+
+    std::unique_ptr<WPushButton> createButton( const char* classname , const WString& name );
+
 };
 
 #endif // TALEPYONETIM_H
