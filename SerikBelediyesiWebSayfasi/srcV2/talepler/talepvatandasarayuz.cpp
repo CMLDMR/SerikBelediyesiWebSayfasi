f663c3dd221ba69169102260a12eba3c82c80b4e
Cemal DEMİR <cemaldemir07@gmail.com>
2019-11-19 14:19:12 +0300

    Talep/Şikayet Listelendi ve Talep Görüntüleme Tamamlandı.

--- SerikBelediyesiWebSayfasi/srcV2/talepler/talepvatandasarayuz.cpp
+++ SerikBelediyesiWebSayfasi/srcV2/talepler/talepvatandasarayuz.cpp
@@ -1,6 +1,56 @@
 #include "talepvatandasarayuz.h"
+#include "taleplistwidget.h"
+#include "talepview.h"
 
-TalepVatandasArayuz::TalepVatandasArayuz()
+
+
+TalepVatandasArayuz::TalepVatandasArayuz(mongocxx::database *_db, bsoncxx::document::value *_userValue)
+    :SerikBLDCore::TalepManager (_db),mTCUser(_userValue)
 {
+    this->initList ();
+}
 
+void TalepVatandasArayuz::initList()
+{
+    this->Content ()->clear ();
+    auto cursor = this->findTalep (SerikBLDCore::Talep().setTCOID (mTCUser.oid ().value ().to_string ().c_str ()));
+    for( auto item : cursor )
+    {
+        this->Content ()->addWidget (cpp14::make_unique<TalepListWidget>(item.oid ().toStdString (),
+                                                                          item.mahalle ().toStdString (),
+                                                                          item.tarih ().toStdString (),
+                                                                          item.durum ().toStdString (),
+                                                                         item.durumColor ().toStdString ()))->ClickItem ().connect ([=](std::string oid){
+            this->initTalep (oid);
+        });
+    }
+}
+
+void TalepVatandasArayuz::initTalep(const std::string &oid)
+{
+    SerikBLDCore::Talep filter;
+    filter.setOid (oid);
+
+    auto mTalep = this->findOneTalep (filter);
+
+    if( !mTalep )
+    {
+        this->showMessage ("Uyarı","Bu Talep Yüklenemedi. Lütfen Daha Sonra Tekrar Deneyiniz");
+    }else{
+        this->Content ()->clear ();
+
+        {
+            auto container = this->Content ()->addWidget (cpp14::make_unique<WContainerWidget>());
+            container->setContentAlignment (AlignmentFlag::Center);
+            container->addWidget (cpp14::make_unique<WText>("Listeye Geri Dön"));
+            container->addStyleClass (Bootstrap::ContextualBackGround::bg_primary);
+            container->clicked ().connect (this,&TalepVatandasArayuz::initList );
+        }
+
+        auto talepView = this->Content ()->addWidget (cpp14::make_unique<TalepView>(std::move(mTalep),
+                                                                                    this->db (),
+                                                                                    &this->mTCUser));
+
+
+    }
 }
--- SerikBelediyesiWebSayfasi/srcV2/talepler/talepvatandasarayuz.h
+++ SerikBelediyesiWebSayfasi/srcV2/talepler/talepvatandasarayuz.h
@@ -1,11 +1,25 @@
 #ifndef TALEPVATANDASARAYUZ_H
 #define TALEPVATANDASARAYUZ_H
 
+#include "Talep/talepmanager.h"
+#include "SerikBelediyesiWebSayfasi/BaseClass/containerwiget.h"
+#include "tc.h"
 
-class TalepVatandasArayuz : public ContainerWidget , public TalepManager
+
+class TalepVatandasArayuz : public ContainerWidget , public SerikBLDCore::TalepManager
 {
 public:
-    TalepVatandasArayuz();
+    TalepVatandasArayuz(mongocxx::database* _db , bsoncxx::document::value* _userValue );
+
+
+
+    void initList();
+
+    void initTalep( const std::string &oid );
+
+
+private:
+    SerikBLDCore::TC mTCUser;
 };
 
 #endif // TALEPVATANDASARAYUZ_H
