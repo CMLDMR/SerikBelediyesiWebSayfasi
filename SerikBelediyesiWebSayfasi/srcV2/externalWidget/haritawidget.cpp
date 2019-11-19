b26b7e3bb791b6b99736819e0415af600c050e3e
Cemal DEMİR <cemaldemir07@gmail.com>
2019-11-19 10:15:36 +0300

    Serik Belediyesi Konumu İçin Tamamlandı.

--- SerikBelediyesiWebSayfasi/srcV2/externalWidget/haritawidget.cpp
+++ SerikBelediyesiWebSayfasi/srcV2/externalWidget/haritawidget.cpp
@@ -1,6 +1,35 @@
 #include "haritawidget.h"
 
+
+const WLeafletMap::Coordinate HaritaWidget::SERIKBLD_COORDS(36.91623,31.104);
+
 HaritaWidget::HaritaWidget()
 {
+    auto layout = setLayout(Wt::cpp14::make_unique<Wt::WHBoxLayout>());
+    setHeight(400);
+    map_ = layout->addWidget(Wt::cpp14::make_unique<Wt::WLeafletMap>(), 1);
+    Wt::Json::Object options;
+    options["maxZoom"] = 19;
+    options["attribution"] = "&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors";
+    map_->addTileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", options);
+    map_->panTo(SERIKBLD_COORDS);
+    setAttributeValue (Style::style,Style::background::color::color (Style::color::White::White));
 
+
+
+}
+
+void HaritaWidget::addSerikLogoMarker()
+{
+    auto emwebLogo = Wt::cpp14::make_unique<Wt::WImage>(
+              Wt::WLink("img/serikbldcoor.png"));
+        emwebLogo->setInline(false);
+        emwebLogo->resize(64, 75);
+        auto emwebMarker = Wt::cpp14::make_unique<Wt::WLeafletMap::WidgetMarker>(SERIKBLD_COORDS, std::move(emwebLogo));
+        map_->addMarker(std::move(emwebMarker));
+}
+
+void HaritaWidget::setZoomLevel(const int &level)
+{
+    map_->setZoomLevel (level);
 }
--- SerikBelediyesiWebSayfasi/srcV2/externalWidget/haritawidget.h
+++ SerikBelediyesiWebSayfasi/srcV2/externalWidget/haritawidget.h
@@ -1,11 +1,25 @@
 #ifndef HARITAWIDGET_H
 #define HARITAWIDGET_H
 
+#include "SerikBelediyesiWebSayfasi/BaseClass/containerwiget.h"
+
 
 class HaritaWidget : public ContainerWidget
 {
 public:
     HaritaWidget();
+
+    void addSerikLogoMarker();
+
+    void setZoomLevel( const int& level );
+
+private:
+    Wt::WLeafletMap *map_;
+
+
+    static const Wt::WLeafletMap::Coordinate SERIKBLD_COORDS;
+
+
 };
 
 #endif // HARITAWIDGET_H
