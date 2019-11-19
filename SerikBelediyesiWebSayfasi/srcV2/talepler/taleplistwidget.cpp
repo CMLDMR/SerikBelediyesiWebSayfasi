8bc355f1e564010406e22d103c742d102459595a
Cemal DEMİR <cemaldemir07@gmail.com>
2019-11-19 11:30:38 +0300

    Tamamlandı.

--- SerikBelediyesiWebSayfasi/srcV2/talepler/taleplistwidget.cpp
+++ SerikBelediyesiWebSayfasi/srcV2/talepler/taleplistwidget.cpp
@@ -1,6 +1,52 @@
 #include "taleplistwidget.h"
 
-TalepListWidget::TalepListWidget()
+TalepListWidget::TalepListWidget(const std::string &oid,
+                                 const std::string &mahalle,
+                                 const std::string &tarih,
+                                 const std::string &durum,
+                                 const std::string &durumColor)
+    :mOid(oid),
+      mMahalle(mahalle),
+      mTarih(tarih),
+      mDurum(durum),
+      mDurumColor(durumColor)
 {
+    addStyleClass (Bootstrap::Grid::Large::col_lg_3
+                                      +Bootstrap::Grid::Medium::col_md_4
+                                      +Bootstrap::Grid::Small::col_sm_6
+                                      +Bootstrap::Grid::ExtraSmall::col_xs_12);
 
+    clicked ().connect ([=](){
+                _clickItem.emit (mOid);
+            });
+
+    {
+        auto _container = addWidget (cpp14::make_unique<WContainerWidget>());
+        _container->addWidget (cpp14::make_unique<WText>(mTarih));
+        _container->setWidth (WLength(std::to_string (100.0)+"%"));
+    }
+    {
+        auto _container = addWidget (cpp14::make_unique<WContainerWidget>());
+        _container->addWidget (cpp14::make_unique<WText>(mMahalle));
+        _container->setWidth (WLength(std::to_string (100.0)+"%"));
+    }
+
+    {
+        auto _container = addWidget (cpp14::make_unique<WContainerWidget>());
+        _container->addWidget (cpp14::make_unique<WText>(mDurum));
+        _container->setWidth (WLength(std::to_string (100.0)+"%"));
+        _container->setAttributeValue (Style::style,Style::background::color::color (durumColor+";")+
+                                       Style::color::color (Style::color::White::Snow)+
+                                       Style::font::weight::bold);
+        _container->addStyleClass (Bootstrap::ImageShape::img_thumbnail);
+
+    }
+    decorationStyle ().setCursor (Cursor::PointingHand);
+    addStyleClass (Bootstrap::ImageShape::img_thumbnail);
+    setMargin (5,Side::Top|Side::Bottom);
+}
+
+Signal<std::string> &TalepListWidget::ClickItem()
+{
+    return _clickItem;
 }
--- SerikBelediyesiWebSayfasi/srcV2/talepler/taleplistwidget.h
+++ SerikBelediyesiWebSayfasi/srcV2/talepler/taleplistwidget.h
@@ -1,11 +1,30 @@
 #ifndef TALEPLISTWIDGET_H
 #define TALEPLISTWIDGET_H
 
+#include "SerikBelediyesiWebSayfasi/BaseClass/containerwiget.h"
 
 class TalepListWidget : public ContainerWidget
 {
 public:
-    TalepListWidget();
+    TalepListWidget(const std::string& oid,
+                    const std::string& mahalle,
+                    const std::string& tarih,
+                    const std::string& durum,
+                    const std::string& durumColor);
+
+
+    Signal<std::string> &ClickItem();
+
+private:
+    std::string mOid;
+
+    std::string mMahalle;
+    std::string mTarih;
+    std::string mDurum;
+    std::string mDurumColor;
+
+
+    Signal<std::string> _clickItem;
 };
 
 #endif // TALEPLISTWIDGET_H
