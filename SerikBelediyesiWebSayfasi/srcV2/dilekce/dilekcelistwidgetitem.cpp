83d689163993cfda0a14c27aae6d0f566aea6c5f
Cemal DEMİR <cemaldemir07@gmail.com>
2019-11-21 18:26:58 +0300

    DilekceListWidgetItem oluşturuldu.

---
+++ SerikBelediyesiWebSayfasi/srcV2/dilekce/dilekcelistwidgetitem.cpp
@@ -0,0 +1,6 @@
+#include "dilekcelistwidgetitem.h"
+
+DilekceListWidgetItem::DilekceListWidgetItem()
+{
+
+}
---
+++ SerikBelediyesiWebSayfasi/srcV2/dilekce/dilekcelistwidgetitem.h
@@ -0,0 +1,11 @@
+#ifndef DILEKCELISTWIDGETITEM_H
+#define DILEKCELISTWIDGETITEM_H
+
+
+class DilekceListWidgetItem : public ContainerWidget
+{
+public:
+    DilekceListWidgetItem();
+};
+
+#endif // DILEKCELISTWIDGETITEM_H
