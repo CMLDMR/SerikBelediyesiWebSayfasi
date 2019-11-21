33b22842aa78cac8d9e641987b2589f958beaa54
Cemal DEMİR <cemaldemir07@gmail.com>
2019-11-21 18:27:26 +0300

    Tamamlandı.

--- SerikBelediyesiWebSayfasi/srcV2/dilekce/dilekcelistwidgetitem.h
+++ SerikBelediyesiWebSayfasi/srcV2/dilekce/dilekcelistwidgetitem.h
@@ -1,11 +1,18 @@
 #ifndef DILEKCELISTWIDGETITEM_H
 #define DILEKCELISTWIDGETITEM_H
 
+#include "SerikBelediyesiWebSayfasi/BaseClass/containerwiget.h"
+#include "dilekce.h"
 
 class DilekceListWidgetItem : public ContainerWidget
 {
 public:
-    DilekceListWidgetItem();
+    DilekceListWidgetItem( const SerikBLDCore::Dilekce dilekce );
+
+
+    Signal<std::string> &ClickDilekceWidget();
+private:
+    Signal<std::string> _ClickDilekce;
 };
 
 #endif // DILEKCELISTWIDGETITEM_H
