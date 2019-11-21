d84402ee3f54d28f5f0bf21f4b922bd76c4be744
Cemal DEMİR <cemaldemir07@gmail.com>
2019-11-21 18:27:34 +0300

    Tamamlandı.

--- SerikBelediyesiWebSayfasi/srcV2/dilekce/dilekcetcyonetim.h
+++ SerikBelediyesiWebSayfasi/srcV2/dilekce/dilekcetcyonetim.h
@@ -1,11 +1,34 @@
 #ifndef DILEKCETCYONETIM_H
 #define DILEKCETCYONETIM_H
 
+#include "SerikBelediyesiWebSayfasi/BaseClass/containerwiget.h"
+
+#include <vector>
+#include <memory>
+#include "dilekcemanager.h"
+#include "dilekce.h"
+#include "tc.h"
+#include "dilekcelistwidgetitem.h"
+#include "dilekceview.h"
 
 class DilekceTCYonetim : public ContainerWidget , public SerikBLDCore::DilekceManager
 {
 public:
-    DilekceTCYonetim();
+    DilekceTCYonetim(mongocxx::database* _db , bsoncxx::document::value *_user);
+
+
+    void list();
+
+    void listBilgiDilekce( const SerikBLDCore::Dilekce &filterDilekce );
+
+    void initBilgiDilekce( const std::string &dilekceOid );
+
+
+
+private:
+    SerikBLDCore::TC mTCUser;
+
+    Signal<std::string> _clickDilekceItem;
 };
 
 #endif // DILEKCETCYONETIM_H
