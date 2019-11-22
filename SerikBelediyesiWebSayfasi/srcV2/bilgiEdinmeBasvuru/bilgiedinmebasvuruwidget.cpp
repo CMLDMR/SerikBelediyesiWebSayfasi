80b910f01995062bbfa6c545ab258a53d6dc40da
Cemal DEMİR <cemaldemir07@gmail.com>
2019-11-22 15:47:28 +0300

    BilgiEdinmeBasvuruWidget Oluşturuldu.

---
+++ SerikBelediyesiWebSayfasi/srcV2/bilgiEdinmeBasvuru/bilgiedinmebasvuruwidget.cpp
@@ -0,0 +1,6 @@
+#include "bilgiedinmebasvuruwidget.h"
+
+BilgiEdinmeBasvuruWidget::BilgiEdinmeBasvuruWidget()
+{
+
+}
---
+++ SerikBelediyesiWebSayfasi/srcV2/bilgiEdinmeBasvuru/bilgiedinmebasvuruwidget.h
@@ -0,0 +1,11 @@
+#ifndef BILGIEDINMEBASVURUWIDGET_H
+#define BILGIEDINMEBASVURUWIDGET_H
+
+
+class BilgiEdinmeBasvuruWidget : public ContainerWidget , SerikBLDCore::BilgiEdinme::BilgiEdinmeManager
+{
+public:
+    BilgiEdinmeBasvuruWidget();
+};
+
+#endif // BILGIEDINMEBASVURUWIDGET_H
