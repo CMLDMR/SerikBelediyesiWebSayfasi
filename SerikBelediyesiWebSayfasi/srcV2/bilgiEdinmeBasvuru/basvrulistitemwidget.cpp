1ce2483daff16716849e813104b735d219811c46
Cemal DEMİR <cemaldemir07@gmail.com>
2019-11-28 19:06:40 +0300

    Kısmen Tamamlandı.

--- SerikBelediyesiWebSayfasi/srcV2/bilgiEdinmeBasvuru/basvrulistitemwidget.cpp
+++ SerikBelediyesiWebSayfasi/srcV2/bilgiEdinmeBasvuru/basvrulistitemwidget.cpp
@@ -1,6 +1,11 @@
 #include "basvrulistitemwidget.h"
+#include "bilgiEdinme/bilgiedinmeitem.h"
 
-BasvruListItemWidget::BasvruListItemWidget()
+BasvruListItemWidget::BasvruListItemWidget(SerikBLDCore::BilgiEdinme::BilgiEdinmeItem *_item)
+    :ContainerWidget (""),item(_item)
 {
-
+    addStyleClass (Bootstrap::ImageShape::img_thumbnail);
+    addWidget (cpp14::make_unique<WText>(item->konu ().toStdString ()));
+    addWidget (cpp14::make_unique<WBreak>());
+    addWidget (cpp14::make_unique<WText>(item->saat ().toStdString ()));
 }
--- SerikBelediyesiWebSayfasi/srcV2/bilgiEdinmeBasvuru/basvrulistitemwidget.h
+++ SerikBelediyesiWebSayfasi/srcV2/bilgiEdinmeBasvuru/basvrulistitemwidget.h
@@ -1,11 +1,21 @@
 #ifndef BASVRULISTITEMWIDGET_H
 #define BASVRULISTITEMWIDGET_H
 
+#include "SerikBelediyesiWebSayfasi/BaseClass/containerwiget.h"
+
+namespace SerikBLDCore {
+namespace BilgiEdinme {
+class BilgiEdinmeItem;
+}
+}
 
 class BasvruListItemWidget : public ContainerWidget
 {
 public:
-    BasvruListItemWidget();
+    BasvruListItemWidget(SerikBLDCore::BilgiEdinme::BilgiEdinmeItem *_item);
+
+    SerikBLDCore::BilgiEdinme::BilgiEdinmeItem *item;
+
 };
 
 #endif // BASVRULISTITEMWIDGET_H
