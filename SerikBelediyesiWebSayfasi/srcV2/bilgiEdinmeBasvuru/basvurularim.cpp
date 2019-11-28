5560480db7255d97b99daeb3fdef0277f5c1692b
Cemal DEMİR <cemaldemir07@gmail.com>
2019-11-28 14:29:06 +0300

    Başvurularım Oluşturuldu.

---
+++ SerikBelediyesiWebSayfasi/srcV2/bilgiEdinmeBasvuru/basvurularim.cpp
@@ -0,0 +1,6 @@
+#include "basvurularim.h"
+
+Basvurularim::Basvurularim()
+{
+
+}
---
+++ SerikBelediyesiWebSayfasi/srcV2/bilgiEdinmeBasvuru/basvurularim.h
@@ -0,0 +1,11 @@
+#ifndef BASVURULARIM_H
+#define BASVURULARIM_H
+
+
+class Basvurularim : public ContainerWidget , public SerikBLDCore::BilgiEdinme::BilgiEdinmeManager
+{
+public:
+    Basvurularim();
+};
+
+#endif // BASVURULARIM_H
