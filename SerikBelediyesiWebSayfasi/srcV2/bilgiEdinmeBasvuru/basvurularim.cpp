0744e60d259dc252997bd56bfa1441747aa6e543
Cemal DEMİR <cemaldemir07@gmail.com>
2019-11-28 19:06:32 +0300

    Kısmen Tamamlandı.

--- SerikBelediyesiWebSayfasi/srcV2/bilgiEdinmeBasvuru/basvurularim.cpp
+++ SerikBelediyesiWebSayfasi/srcV2/bilgiEdinmeBasvuru/basvurularim.cpp
@@ -1,6 +1,24 @@
 #include "basvurularim.h"
 
-Basvurularim::Basvurularim()
+V2::Basvurularim::Basvurularim(DB *_db, const SerikBLDCore::TC &_tcUser)
+    :ContainerWidget ("Başvurularım"),SerikBLDCore::BilgiEdinme::BilgiEdinmeManager (_db)
 {
+
+
+
+
+    auto cursor = this->UpdateList (SerikBLDCore::BilgiEdinme::BilgiEdinmeItem().setTCoid (_tcUser.oid ().value ().to_string ().c_str ()));
+
+
+
+
+
+    for( auto __item : cursor )
+    {
+        auto item = this->Content ()->addWidget (cpp14::make_unique<BasvruListItemWidget>(&__item));
+        item->addStyleClass (Bootstrap::Grid::Large::col_lg_3);
+    }
+
+
 
 }
--- SerikBelediyesiWebSayfasi/srcV2/bilgiEdinmeBasvuru/basvurularim.h
+++ SerikBelediyesiWebSayfasi/srcV2/bilgiEdinmeBasvuru/basvurularim.h
@@ -1,11 +1,18 @@
 #ifndef BASVURULARIM_H
 #define BASVURULARIM_H
 
+#include "SerikBelediyesiWebSayfasi/BaseClass/containerwiget.h"
+#include "bilgiEdinme/bilgiedinmemanager.h"
+#include "tc.h"
+#include "basvrulistitemwidget.h"
 
+namespace V2 {
 class Basvurularim : public ContainerWidget , public SerikBLDCore::BilgiEdinme::BilgiEdinmeManager
 {
 public:
-    Basvurularim();
+    Basvurularim(DB* _db , const SerikBLDCore::TC& _tcUser);
 };
+}
+
 
 #endif // BASVURULARIM_H
