097f0cc5ea4825533c1aa1b18ad5c7fd06ea6cbb
Cemal DEMİR <cemaldemir07@gmail.com>
2019-11-28 19:05:49 +0300

    BasvuruListWidget oluşturuldu.

---
+++ SerikBelediyesiWebSayfasi/srcV2/bilgiEdinmeBasvuru/basvrulistitemwidget.cpp
@@ -0,0 +1,6 @@
+#include "basvrulistitemwidget.h"
+
+BasvruListItemWidget::BasvruListItemWidget()
+{
+
+}
---
+++ SerikBelediyesiWebSayfasi/srcV2/bilgiEdinmeBasvuru/basvrulistitemwidget.h
@@ -0,0 +1,11 @@
+#ifndef BASVRULISTITEMWIDGET_H
+#define BASVRULISTITEMWIDGET_H
+
+
+class BasvruListItemWidget : public ContainerWidget
+{
+public:
+    BasvruListItemWidget();
+};
+
+#endif // BASVRULISTITEMWIDGET_H
