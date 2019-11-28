42e26ad9bf39d2d45e055de447a6a3a221013a3f
Cemal DEMİR <cemaldemir07@gmail.com>
2019-11-28 13:50:36 +0300

    VatandasWidget Oluşturuldu.

---
+++ SerikBelediyesiWebSayfasi/srcV2/vatandas/vatandaswidget.cpp
@@ -0,0 +1,6 @@
+#include "vatandaswidget.h"
+
+VatandasWidget::VatandasWidget()
+{
+
+}
---
+++ SerikBelediyesiWebSayfasi/srcV2/vatandas/vatandaswidget.h
@@ -0,0 +1,11 @@
+#ifndef VATANDASWIDGET_H
+#define VATANDASWIDGET_H
+
+
+class VatandasWidget : public TC
+{
+public:
+    VatandasWidget();
+};
+
+#endif // VATANDASWIDGET_H
