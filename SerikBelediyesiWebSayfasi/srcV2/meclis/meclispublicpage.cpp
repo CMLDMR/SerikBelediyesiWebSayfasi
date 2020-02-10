ba27493911d5e62cd79218e48e46d498f711813e
Cemal DEMİR <cemaldemir07@gmail.com>
2020-02-10 16:41:23 +0300

    namespace v2 Eklendi.

--- SerikBelediyesiWebSayfasi/srcV2/meclis/meclispublicpage.cpp
+++ SerikBelediyesiWebSayfasi/srcV2/meclis/meclispublicpage.cpp
@@ -1,7 +1,38 @@
 #include "meclispublicpage.h"
 
-MeclisPublicPage::MeclisPublicPage(DB *db)
-    :ContainerWidget("Meclis"),SerikBLDCore::Meclis::MeclisManager (db)
+
+
+v2::MeclisPublicPage::MeclisPublicPage(SerikBLDCore::DB *db)
+    :ContainerWidget("Meclis v2"),SerikBLDCore::Meclis::MeclisManager (db)
 {
     this->setMargin (90,Side::Top);
+
+
+    SerikBLDCore::FindOptions findOptions;
+
+    findOptions.setSort (SerikBLDCore::Item("none").append("_id",1));
+
+
+    this->UpdateList (SerikBLDCore::Meclis::MeclisItem(),findOptions);
+}
+
+void v2::MeclisPublicPage::onList(const QVector<SerikBLDCore::Meclis::MeclisItem> *mlist)
+{
+
+    Content ()->clear ();
+
+
+    for( auto item : *mlist )
+    {
+        auto container = Content ()->addWidget (cpp14::make_unique<WContainerWidget>());
+        container->addStyleClass (Bootstrap::Grid::Large::col_lg_3+
+                                  Bootstrap::Grid::Medium::col_md_4+
+                                  Bootstrap::Grid::Small::col_sm_6+
+                                  Bootstrap::Grid::ExtraSmall::col_xs_6+
+                                  Bootstrap::ImageShape::img_thumbnail);
+
+        container->addWidget (cpp14::make_unique<WText>(item.ay ().toStdString ()));
+    }
+
+
 }
--- SerikBelediyesiWebSayfasi/srcV2/meclis/meclispublicpage.h
+++ SerikBelediyesiWebSayfasi/srcV2/meclis/meclispublicpage.h
@@ -4,10 +4,17 @@
 #include "meclis/meclismanager.h"
 #include "SerikBelediyesiWebSayfasi/BaseClass/containerwiget.h"
 
+namespace v2{
+
 class MeclisPublicPage : public ContainerWidget , public SerikBLDCore::Meclis::MeclisManager
 {
 public:
-    MeclisPublicPage(SerikBLDCore::DB* db);
+    explicit MeclisPublicPage(SerikBLDCore::DB* db);
+
+    void onList(const QVector<SerikBLDCore::Meclis::MeclisItem> *mlist) override;
 };
+
+}
+
 
 #endif // MECLISPUBLICPAGE_H
