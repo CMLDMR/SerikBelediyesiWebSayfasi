980714e22ecef82560f44e6393214e9c0d727560
Cemal DEMİR <cemaldemir07@gmail.com>
2020-02-14 12:58:42 +0300

    Meclis Sayfasında Meclis Bilgileri Eklendi.

--- SerikBelediyesiWebSayfasi/srcV2/meclis/meclispublicpage.cpp
+++ SerikBelediyesiWebSayfasi/srcV2/meclis/meclispublicpage.cpp
@@ -6,11 +6,14 @@ v2::MeclisPublicPage::MeclisPublicPage(SerikBLDCore::DB *db)
     :ContainerWidget("Meclis v2"),SerikBLDCore::Meclis::MeclisManager (db)
 {
     this->setMargin (90,Side::Top);
+    setAttributeValue (Style::style,Style::background::color::color (Style::color::White::Snow));
+
+    wApp->setInternalPath (WString(MeclisLinks::mainPage).toUTF8 ());
 
 
     SerikBLDCore::FindOptions findOptions;
 
-    findOptions.setSort (SerikBLDCore::Item("none").append("_id",1));
+    findOptions.setSort (SerikBLDCore::Meclis::MeclisItem().append(SerikBLDCore::Meclis::Key::julianDate,-1));
 
 
     this->UpdateList (SerikBLDCore::Meclis::MeclisItem(),findOptions);
@@ -20,18 +23,52 @@ void v2::MeclisPublicPage::onList(const QVector<SerikBLDCore::Meclis::MeclisItem
 {
 
     Content ()->clear ();
+    Content ()->setContentAlignment (AlignmentFlag::Center);
 
-
+    std::int32_t year = 0;
+    int colorBeginRange = 175 , colorEndRange = 225;
+    std::string color = Style::background::color::rgb (this->getRandom (colorBeginRange,colorEndRange),this->getRandom (colorBeginRange,colorEndRange),this->getRandom (colorBeginRange,colorEndRange));
     for( auto item : *mlist )
     {
-        auto container = Content ()->addWidget (cpp14::make_unique<WContainerWidget>());
-        container->addStyleClass (Bootstrap::Grid::Large::col_lg_3+
-                                  Bootstrap::Grid::Medium::col_md_4+
-                                  Bootstrap::Grid::Small::col_sm_6+
-                                  Bootstrap::Grid::ExtraSmall::col_xs_6+
-                                  Bootstrap::ImageShape::img_thumbnail);
+        auto _container = Content ()->addWidget (cpp14::make_unique<WContainerWidget>());
+        _container->addStyleClass (Bootstrap::Grid::col_full_12);
+        _container->setContentAlignment (AlignmentFlag::Center);
+        auto container = _container->addWidget (cpp14::make_unique<WContainerWidget>());
+        container->setMaximumSize (1024,WLength::Auto);
+        if( year != item.yil () )
+        {
+            year = item.yil ();
+            color = Style::background::color::rgb (this->getRandom (colorBeginRange,colorEndRange),this->getRandom (colorBeginRange,colorEndRange),this->getRandom (colorBeginRange,colorEndRange));
+            container->setMargin (15,Side::Top);
+        }
+        container->setAttributeValue (Style::style,color);
+        container->addStyleClass (Bootstrap::ImageShape::img_thumbnail);
+        container->setWidth (WLength("100%"));
 
-        container->addWidget (cpp14::make_unique<WText>(item.ay ().toStdString ()));
+
+
+        container->setContentAlignment (AlignmentFlag::Center);
+        WLink link = WLink(LinkType::Url,"?type=gundem&_id="+item.oid ().value ().to_string ());
+        link.setTarget(Wt::LinkTarget::NewWindow);
+
+        std::unique_ptr<Wt::WAnchor> anchor =
+                Wt::cpp14::make_unique<Wt::WAnchor>(link,
+                                std::to_string (item.yil ())+" "+item.ay ().toStdString ());
+        container->addWidget (std::move(anchor));
+
+        container->addStyleClass (Bootstrap::ContextualBackGround::bg_info);
+
+
+
+
+//        auto text = container->addWidget (cpp14::make_unique<WText>(std::to_string (item.yil ())+" "+item.ay ().toStdString ()));
+//        text->setAttributeValue (Style::style,Style::color::color (Style::color::White::Snow)+Style::font::weight::bold);
+
+//        container->decorationStyle ().setCursor (Cursor::PointingHand);
+
+//        container->clicked ().connect ([=](){
+//            wApp->setInternalPath ("/?test=12&Value=2233");
+//        });
     }
 
 
--- SerikBelediyesiWebSayfasi/srcV2/meclis/meclispublicpage.h
+++ SerikBelediyesiWebSayfasi/srcV2/meclis/meclispublicpage.h
@@ -6,12 +6,27 @@
 
 namespace v2{
 
+
+namespace MeclisLinks {
+
+static const std::string mainPage{"/meclis"};
+
+static const std::string meclisSelect( const std::string& meclis)
+{
+    return mainPage+"&select="+meclis;
+}
+
+}
+
 class MeclisPublicPage : public ContainerWidget , public SerikBLDCore::Meclis::MeclisManager
 {
 public:
     explicit MeclisPublicPage(SerikBLDCore::DB* db);
 
     void onList(const QVector<SerikBLDCore::Meclis::MeclisItem> *mlist) override;
+
+
+
 };
 
 }
