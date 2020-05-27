1720960a7b8e6444905f7958d125f3ac75a83db4
Cemal DEMİR <cemaldemir07@gmail.com>
2020-05-27 15:08:16 +0300

    MimarProjeView Eklendi.

---
+++ SerikBelediyesiWebSayfasi/srcV2/imar/mimarprojeview.cpp
@@ -0,0 +1,6 @@
+#include "mimarprojeview.h"
+
+MimarProjeView::MimarProjeView()
+{
+
+}
---
+++ SerikBelediyesiWebSayfasi/srcV2/imar/mimarprojeview.h
@@ -0,0 +1,11 @@
+#ifndef MIMARPROJEVIEW_H
+#define MIMARPROJEVIEW_H
+
+
+class MimarProjeView : public SerikBLDCore::Imar::MimariProje::MimariProje, public ContainerWidget
+{
+public:
+    MimarProjeView();
+};
+
+#endif // MIMARPROJEVIEW_H
