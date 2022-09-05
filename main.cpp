5a452f4012ccd997a1a8d8aa9690f832b10f846f
Cemal Demir <CMLDMR@users.noreply.github.com>
2022-09-05 15:14:23 +0300

    Değişiklik Yok

--- main.cpp
+++ main.cpp
@@ -12,94 +12,97 @@
 
 #include <string>
 #include <bitset>
+
+#include <ranges>
+
 ///https://www.flaticon.com/packs/essential-collection
 /// https://codepen.io/asfarmed/pen/cfslr
 ///
 ///
 
-std::unique_ptr<Wt::WApplication> createApplication(const Wt::WEnvironment& env)
-{
-    auto app = cpp14::make_unique<MainApplication>(env);
-    return app;
-}
+//std::unique_ptr<Wt::WApplication> createApplication(const Wt::WEnvironment& env)
+//{
+//    auto app = cpp14::make_unique<MainApplication>(env);
+//    return app;
+//}
 
 
-class PlaintextResource : public Wt::WResource {
-    int counter = 0;
+//class PlaintextResource : public Wt::WResource {
+//    int counter = 0;
 
 
-public:
-    PlaintextResource(){
+//public:
+//    PlaintextResource(){
 
-        this->dataChanged ().connect ([=](){
-            std::cout << "DATA Changed" << std::endl;
-        });
+//        this->dataChanged ().connect ([=](){
+//            std::cout << "DATA Changed" << std::endl;
+//        });
 
-        this->dataExceeded ().connect ([=]( const uint64_t size ){
-            std::cout << "File Size Exceed: " << size << std::endl;
-        });
+//        this->dataExceeded ().connect ([=]( const uint64_t size ){
+//            std::cout << "File Size Exceed: " << size << std::endl;
+//        });
 
-        this->dataReceived ().connect ([=](const uint64_t& size, const uint64_t& data ){
-            std::cout << "File Size: " << size << " Size DataSize: " << data << std::endl;
-        });
-    }
-
-//    std::string fromIstream(std::istream &stream)
-//    {
-//        std::istreambuf_iterator<char> eos;
-//         std::cout << std::string(std::istreambuf_iterator<char>(stream), eos) << "\n";
-//        return "";
+//        this->dataReceived ().connect ([=](const uint64_t& size, const uint64_t& data ){
+//            std::cout << "File Size: " << size << " Size DataSize: " << data << std::endl;
+//        });
 //    }
 
-    QByteArray fromIstream(std::istream &stream)
-    {
-        QByteArray ar;
-        char c;
-        while(stream.read(&c,1) && !stream.eof() ){
-            ar.append(c);
-        }
-        return ar;
-    }
+////    std::string fromIstream(std::istream &stream)
+////    {
+////        std::istreambuf_iterator<char> eos;
+////         std::cout << std::string(std::istreambuf_iterator<char>(stream), eos) << "\n";
+////        return "";
+////    }
 
-    virtual void handleRequest(const Wt::Http::Request &request, Wt::Http::Response &response) {
+//    QByteArray fromIstream(std::istream &stream)
+//    {
+//        QByteArray ar;
+//        char c;
+//        while(stream.read(&c,1) && !stream.eof() ){
+//            ar.append(c);
+//        }
+//        return ar;
+//    }
 
-
-        std::cout << "Method: "<<request.method () << std::endl;
-
-        long long length = 0;
-
-        for( const auto &item : request.headers () ){
-            std::cout << "name: "<<item.name () << " value: " << item.value () << std::endl;
-            if( item.name() == "Content-Length" ){
-                length = std::stoull(item.value());
-            }
-        }
-
-        auto var = fromIstream(request.in());
+//    virtual void handleRequest(const Wt::Http::Request &request, Wt::Http::Response &response) {
 
 
-        bsoncxx::document::view view{(std::uint8_t*)var.data(),static_cast<size_t>(var.size())};
+//        std::cout << "Method: "<<request.method () << std::endl;
+
+//        long long length = 0;
+
+//        for( const auto &item : request.headers () ){
+//            std::cout << "name: "<<item.name () << " value: " << item.value () << std::endl;
+//            if( item.name() == "Content-Length" ){
+//                length = std::stoull(item.value());
+//            }
+//        }
+
+//        auto var = fromIstream(request.in());
 
 
-        std::cout << "path: " << request.path () << std::endl;
-
-        for( const auto &item : request.getParameterMap () ){
-            auto str = item.first;
-            std::cout << str << "  " << item.second.size() << std::endl;
-            for( const auto &_char : item.second ){
-                std::cout << _char;
-            }
-            std::cout << "\n";
-            std::cout << str.c_str()<<"\n";
-//             response.out() << str;
-        }
+//        bsoncxx::document::view view{(std::uint8_t*)var.data(),static_cast<size_t>(var.size())};
 
 
-        response.out().write((const char*)view.data(),view.length());
+//        std::cout << "path: " << request.path () << std::endl;
+
+//        for( const auto &item : request.getParameterMap () ){
+//            auto str = item.first;
+//            std::cout << str << "  " << item.second.size() << std::endl;
+//            for( const auto &_char : item.second ){
+//                std::cout << _char;
+//            }
+//            std::cout << "\n";
+//            std::cout << str.c_str()<<"\n";
+////             response.out() << str;
+//        }
 
 
-    }
-};
+//        response.out().write((const char*)view.data(),view.length());
+
+
+//    }
+//};
 
 
 
@@ -108,6 +111,9 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     mongocxx::instance ins{};
+
+    std::cout << __cplusplus << "\n";
+
 
 //    try {
 //        int argc = 6;
