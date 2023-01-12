10208a0cbf3b5cfeae0306b27c82ec55b89f36fc
Cemal Demir <CMLDMR@users.noreply.github.com>
2023-01-12 12:55:30 +0300

    Yeni Dialog Widget Eklendi.

--- SerikBelediyesiWebSayfasi/BaseClass/containerwiget.cpp
+++ SerikBelediyesiWebSayfasi/BaseClass/containerwiget.cpp
@@ -232,6 +232,26 @@ void ContainerWidget::removeDialog(WDialog *mDialog)
     wApp->instance()->root()->removeChild(mDialog);
 }
 
+DialogContainerWidget* ContainerWidget::createFlatDialog(const std::string &title)
+{
+    auto container = Wt::WApplication::instance()->root()->addNew<DialogContainerWidget>(title);
+    container->setPositionScheme(PositionScheme::Fixed);
+    container->addStyleClass(CSSStyle::NewDialog::newDialog);
+    container->setContentAlignment(AlignmentFlag::Center);
+
+    container->Rejected().connect([=](){
+        Wt::WApplication::instance()->root()->removeWidget(container);
+    });
+
+
+    return container;
+}
+
+void ContainerWidget::removeDialog(DialogContainerWidget *removedContainer)
+{
+    WApplication::instance()->root()->removeWidget(removedContainer);
+}
+
 void ContainerWidget::setContainerStyle(ContainerWidget::ContainerStyleType type)
 {
 
@@ -443,3 +463,68 @@ Signal<NoClass> &FileUploaderWidget::Uploaded()
 
 
 
+
+DialogContainerWidget::DialogContainerWidget(const std::string &title)
+{
+
+    auto contentContainer = this->addNew<WContainerWidget>();
+    contentContainer->setMaximumSize(768,WLength::Auto);
+    contentContainer->setPositionScheme(PositionScheme::Relative);
+    contentContainer->setHeight(WLength("100%"));
+    contentContainer->setWidth(WLength("100%"));
+
+
+    auto vLayout = contentContainer->setLayout(cpp14::make_unique<WVBoxLayout>());
+    vLayout->addStretch(1);
+
+    auto mTitleContainer = vLayout->addWidget(cpp14::make_unique<WContainerWidget>());
+    mTitleContainer->addNew<WText>(title);
+    mTitleContainer->addStyleClass(Bootstrap::ContextualBackGround::bg_primary);
+
+    mContent = vLayout->addWidget(cpp14::make_unique<WContainerWidget>());
+    mContent->setAttributeValue(Style::style,Style::background::color::color(Style::color::White::AliceBlue));
+    mContent->setMaximumSize(WLength::Auto,WLength::Auto);
+    mContent->addStyleClass(Bootstrap::Grid::row);
+
+
+    auto footer = vLayout->addWidget(cpp14::make_unique<WContainerWidget>());
+    footer->setMaximumSize(WLength::Auto,50);
+    footer->setAttributeValue(Style::style,Style::background::color::rgba(255,255,255,0.65));
+
+    auto hLayout = footer->setLayout(cpp14::make_unique<WHBoxLayout>());
+
+    auto acceptContainer = hLayout->addWidget(cpp14::make_unique<WContainerWidget>());
+    acceptContainer->addStyleClass(Bootstrap::Button::Primary);
+    acceptContainer->setPadding(5,Side::Top|Side::Bottom);
+    acceptContainer->addNew<WText>("<b>Kaydet</b>",TextFormat::UnsafeXHTML);
+    acceptContainer->decorationStyle().setCursor(Cursor::PointingHand);
+
+    acceptContainer->clicked().connect([=](){
+        _Accepted.emit(NoClass());
+    });
+
+    auto closeContainer = hLayout->addWidget(cpp14::make_unique<WContainerWidget>());
+    closeContainer->addStyleClass(Bootstrap::Button::Default);
+    closeContainer->setPadding(5,Side::Top|Side::Bottom);
+    closeContainer->addNew<WText>("<b>Kapat</b>",TextFormat::UnsafeXHTML);
+    closeContainer->decorationStyle().setCursor(Cursor::PointingHand);
+    closeContainer->clicked().connect([=](){
+        _Rejected.emit(NoClass());
+    });
+    vLayout->addStretch(1);
+}
+
+WContainerWidget *DialogContainerWidget::Content()
+{
+    return mContent;
+}
+
+Signal<NoClass> &DialogContainerWidget::Accepted()
+{
+    return _Accepted;
+}
+
+Signal<NoClass> &DialogContainerWidget::Rejected()
+{
+    return _Rejected;
+}
